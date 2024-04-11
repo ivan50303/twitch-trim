20598b2ab2786999eb33f84fc8bfec2c2368d247
Ivan Rojas <irojas1@uncc.edu>
2024-04-11 17:13:13 -0400

    remove await

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ async function runApp() {
   const clips = await getTopClipsOfTheWeek()
   const outputVideoPath = await createVideoFromClips(clips)
   const categoryName = category.name
-  await uploadVideoToYoutube(outputVideoPath, categoryName)
+  uploadVideoToYoutube(outputVideoPath, categoryName)
 }
 
 runApp()
