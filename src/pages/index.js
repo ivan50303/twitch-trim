5e1acd14b2bd5b41ce978b41aaf53629a5a094c9
jensen <jensenlobo30@outlook.com>
2024-04-24 19:04:57 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import GenerateButton from '@/components/generateButton'
+import '../app/globals.css'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>TwitchTrim</h1>
-      <GenerateButton />
-    </div>
+      <div>
+        <h1>TwitchTrim</h1>
+        <GenerateButton />
+      </div>
   )
 }
 
